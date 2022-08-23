d05654590188fca23216f1162d29f04ea7b0d0cb
Asadbekdevuz <mirxoliqovasad@gmail.com>
2022-08-23 22:31:09 +0500

    for fix foreach

--- js/index.js
+++ js/index.js
@@ -10,30 +10,29 @@ let arr = [];
 
 elForm.addEventListener("submit", function (evt) {
   evt.preventDefault()
-
-  let person = {
+  
+  const person = {
     name: elNameInput.value,
     surname: elSurInput.value,
     relative: elRelInput.value,
     phone: elTelInput.value,
   }
+
   elNameInput.value = null;
   elSurInput.value = null;
   elRelInput.value = null;
   elTelInput.value = null;
-  // console.log(person);
+  
   arr.push(person);
-  console.log(arr);
   elList.innerHTML = "";
-  for (let i = 0; i < arr.length; i++) {
+
+  arr.forEach (i => {
     let elItem = document.createElement("li");
     elItem.setAttribute("class", "d-flex justify-content-between align-items-center list-group-item list-group-item-primary"); 
 
-    elItem.innerHTML = `<span>${arr[i].name} </span> <span>${arr[i].surname}</span> <span>${arr[i].relative}</span> <a href="tel:+998991103510">${arr[i].phone}</a> <button type= "button" class="btn btn-danger">delete</button> `;
+    elItem.innerHTML = `<span>${i.name} </span> <span>${i.surname}</span> <span>${i.relative}</span> <a href="tel:">${i.phone}</a> <button type= "button" class="btn btn-danger">delete</button> `;
     elList.appendChild(elItem);
-  }
-
-  console.log(person.name);
+  })
 
 })
 
